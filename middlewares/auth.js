88fdaf572996628f118ea3c5fc22d8dc1ff28ae7
Chetan Chauhan <devilmafia71@gmail.com>
2025-07-10 00:06:43 +0530

    all done boss

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,23 @@
 
+const jwt = require('jsonwebtoken');
 
 exports.auth = async(req,res,next){
     
+    try{
+        const authHeader = req.headers['authorization'];
+
+        const token = hedertoken.split(' ')[1];
+
+        if(!token) return res.status(404).json({message:"Access Denied"});
+
+        const isvalidToken = jwt.verify(token,SECRET_KEY);
+
+        next();
+
+        return res.status(201).json(isvalidToken);
+    }
+    catch(error){
+        console.log("Token not found",error.message);
+        res.status(500).json({message:"server side mei dikkat h"});
+    }
 }
